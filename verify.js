4295259148df56f3501ca8fcbfa0e9986b67e42d
mdyoyo <ht9938@qq.com>
2016-06-10 13:46:23 +0800

    1

---
+++ verify.js
@@ -0,0 +1,25 @@
+var express = require('express');
+var WXBizMsgCrypt = require('wechat-crypto');
+
+var config = {
+    token:'HstHOMAgMci9qtBu2zirJ',
+    encodingAESKey:'wnL0dwXTm8R9zT2GM8xUvPfohjWGHfVp3glagC9zwSB',
+    corpId:'wx1d3765eb45497a18'
+};
+var app = express();
+
+app.get('/wxservice',function(req,res){
+    var msg_signature = req.query.msg_signature;
+    var timestamp = req.query.timestamp;
+    var nonce = req.query.nonce;
+    var echostr = req.query.echostr;
+    var cryptor = new WXBizMsgCrypt(config.token, config.encodingAESKey, config.corpId)
+    var s = cryptor.decrypt(echostr);
+    res.send(s.message);
+});
+
+app.listen(1337);
+console.log('Server running at http://123.206.71.86:1337/');
+//function verifyURL(){
+//
+//}
