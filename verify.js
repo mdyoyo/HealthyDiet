2c716670021a09fd411d3e1ce253ec79298a3f77
mdyoyo <ht9938@qq.com>
2016-06-10 16:28:25 +0800

    1

--- verify.js
+++ verify.js
@@ -1,5 +1,5 @@
 var express = require('express');
-var WXBizMsgCrypt = require('wechat-enterprise');
+//var WXBizMsgCrypt = require('./lib/WXUtils');
 
 var config = {
     token:'JiCTLRjtUNh9PuPt1no1wCQML1rm',
@@ -14,13 +14,57 @@ console.log("hi");
     var timestamp = req.query.timestamp;
     var nonce = req.query.nonce;
     var echostr = req.query.echostr;
-    var cryptor = new WXBizMsgCrypt(config.token, config.encodingAESKey, config.corpId);
-    var s = cryptor.decrypt(echostr);
-    res.send(s.message);
+    console.log(msg_signature);
+    console.log(timestamp);
+    console.log(nonce);
+    console.log(echostr);
+//    var cryptor = new WXBizMsgCrypt(config.token, config.encodingAESKey, config.corpId);
+    var errCode = verifyURL(msg_signature,timestamp,nonce,echostr);
+    if(errCode === 0){
+        var s = decrypt(echostr);
+        res.send(s.message);
+    }else{
+        res.end('fail');
+    }
+
+
 });
 
 app.listen(1337);
 console.log('Server running at http://123.206.71.86:1337/');
-//function verifyURL(){
-//
-//}
+function verifyURL(msgSignature,timeStamp,nonce,echoStr){
+    var errCode;
+    if(config.encodingAESKey.length != 43){
+        errCode = -40004;
+    }
+    var b = new Buffer(config.encodingAESKey+"=", 'base64');
+    var aesKey = b.toString();
+    console.log('aesKey:  '+aesKey);
+    var key = [config.token,timeStamp,nonce,echoStr].sort().join('');
+    var sha1 = require('crypto').createHash('sha1');
+    sha1.update(key);
+
+    if(sha1.digest('hex') == msgSignature){
+        errCode = 0;
+    }else{
+        errCode = -40001;
+    }
+    return errCode;
+}
+function decrypt(text) {
+    // 创建解密对象，AES采用CBC模式，数据采用PKCS#7填充；IV初始向量大小为16字节，取AESKey前16字节
+    var decipher = crypto.createDecipheriv('aes-256-cbc', this.key, this.iv);
+    decipher.setAutoPadding(false);
+    var deciphered = Buffer.concat([decipher.update(text, 'base64'), decipher.final()]);
+
+    deciphered = PKCS7Encoder.decode(deciphered);
+    // 算法：AES_Encrypt[random(16B) + msg_len(4B) + msg + $CorpID]
+    // 去除16位随机数
+    var content = deciphered.slice(16);
+    var length = content.slice(0, 4).readUInt32BE(0);
+
+    return {
+        message: content.slice(4, length + 4).toString(),
+        id: content.slice(length + 4).toString()
+    };
+}
