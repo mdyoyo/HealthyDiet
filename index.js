c254f860164bd4b4edd9e7bc357346f9e7384c94
mdyoyo <ht9938@qq.com>
2016-06-12 22:55:18 +0800

    1

--- index.js
+++ index.js
@@ -18,10 +18,6 @@ var config = {
 var access_token;
 
 function checkSignature(params){
-    //var key = [token,params.timeStamp,params.nonce,params.echoStr].sort().join('');
-    //var sha1 = crypto.createHash('sha1');
-    //sha1.update(key);
-    //return (sha1.digest('hex') == params.msgSignature);
     var cryptor = new WXBizMsgCrypt(config.token, config.encodingAESKey, config.corpId);
     var errCode = cryptor.verifyURL(params.msg_signature,params.timeStamp,params.nonce,params.echoStr);
     if(errCode === 0){
@@ -35,62 +31,51 @@ function checkSignature(params){
 //企业在接收消息，以及发送被动响应消息时，数据包以xml格式组成，以AES方式加密传输
 //接收事件
 var server = http.createServer(function(req,res){
+
     var query = url.parse(req.url).query;//?后面的内容
     var params = qs.parse(query);
-    /**params内容：
-     * { msg_signature: '7093f89c7814f06ebf29b5d38d598101341ceb0a',
+    console.log(params);
+
+    if(req.method == "GET"){//get请求，返回echostr用于通过服务器有效校验
+        /**params内容：
+         * { msg_signature: '7093f89c7814f06ebf29b5d38d598101341ceb0a',
         timestamp: '1465734386',
         nonce: '1055659467',
         echostr: 'AKIyN3y9rLyAilMhDVsJruPTAjMGH/SpG/zlUFd+lZmOotmPcvoZJ8Gu4WEo386X5/K94kCLLSUovSyllbGqbA==' }
-     */
-    var echostr = params.echostr;
-    var cryptor = new WXBizMsgCrypt(config.token, config.encodingAESKey, config.corpId);
-    var errCode = cryptor.verifyURL(params.msg_signature,params.timestamp,params.nonce,echostr);
-    console.log(errCode);
-    //if(errCode === 0){
-    //    /**
-    //     * { message: '3617532078790236155', id: 'wx1d3765eb45497a18' }
-    //     */
-    //    var s = cryptor.decrypt(echostr);//解析出明文
-    //    res.end(s.message);
-    //}
-    //else{
-    //    res.end('fail');
-    //}
-    if(errCode != 0){
-        res.end(fail);
-    }
-    if(req.method == "GET"){
+         */
+        var echostr = params.echostr;
+        var cryptor = new WXBizMsgCrypt(config.token, config.encodingAESKey, config.corpId);
+        var errCode = cryptor.verifyURL(params.msg_signature,params.timestamp,params.nonce,echostr);
+        console.log(errCode);
+        if(errCode != 0){
+            res.end(fail);
+        }
         /**
          * { message: '3617532078790236155', id: 'wx1d3765eb45497a18' }
          */
-        var s = cryptor.decrypt(echostr);//解析出明文
-        res.end(s.message);
+        else{
+            var s = cryptor.decrypt(echostr);
+            //解析出明文
+            res.end(s.message);
+        }
     }
-    //if(!checkSignature(params,config.token)){
-    //    response.end('校验失败');
-    //}
-    //if(request.method == "GET"){
-    //    //get请求，返回echostr用于通过服务器有效校验
-    //    response.end(params.echostr);
-    //}
-    //if(req.method == "POST"){
-    //    var postdata = "";
-    //    request.addListener("data",function(postchunk){
-    //        postdata += postchunk;
-    //    });
-    //    request.addListener("end",function(){
-    //        var parseString = require('xml2js').parseString;
-    //        parseString(postdata, function(err,result){
-    //            if(!err){
-    //                console.log(result);
-    //            }
-    //        });
-    //    });
-    //}
+    else{//post请求，微信发给开发者服务器
+        var postdata = "";
+        request.addListener("data",function(postchunk){
+            postdata += postchunk;
+        });
+        request.addListener("end",function(){
+            var parseString = require('xml2js').parseString;
+            parseString(postdata, function(err,result){
+                if(!err){
+                    console.log(result);
+                }
+            });
+        });
+    }
 
 });
-server.listen(1337);
+server.listen(1338);
 console.log('server running at port 1338');
 
 
